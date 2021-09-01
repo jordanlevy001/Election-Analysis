52129cd32457dc42f9987105f4e5f515564d9255
jordanlevy001 <jordan.levy001@gmail.com>
2021-09-01 15:05:24 -0700

    Adding PyPoll.py file.

---
+++ 03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -0,0 +1,32 @@
+# Create a variable called 'name' that holds a string
+name = ["Jordan"]
+
+# Create a variable called 'country' that holds a string
+country = ["United States of America"]
+
+# Create a variable called 'age' that holds an integer
+age = 26
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 16
+
+# Calculate the daily wage for the user
+daily_wage = 8 * (hourly_wage)
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = True
+
+# Print out "Hello <name>!"
+print("Hello Jordan!")
+
+# Print out what country the user entered
+print(country)
+
+# Print out the user's age
+print(str(age))
+
+# With an f-string, print out the daily wage that was calculated
+print(f"{daily_wage}")
+
+# With an f-string, print out whether the users were satisfied
+print(f"{satisfied}")
